e718d1fc506b2e0fc6163ad162035baaf104a222
SAaronB <programmingsean@gmail.com>
2018-10-23 08:29:11 -0600

    Changed name of view controller and  linked image and label to view controller

--- SeanAlgorithm/Controller/AlgorithmViewController.swift
+++ SeanAlgorithm/Controller/AlgorithmViewController.swift
@@ -8,13 +8,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class AlgorithmViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    @IBOutlet var algorithmText: UILabel!
+    
+    @IBOutlet var swiftImage: UIImageView!
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
